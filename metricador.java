6a77ca69936a6939ac12169b8781d06047fd2a29
Unknown <diefrodriguezcha@unal.edu.co>
2018-05-24 16:50:27 -0500

    hasta donde va

--- metricador.java
+++ metricador.java
@@ -42,6 +42,7 @@ public class metricador extends Java8BaseListener{
       }
       /* Operacionaes de tipo A -> a + b, A -> a - b*/
       @Override public void enterAdditiveExpression(Java8Parser.AdditiveExpressionContext ctx) {
+          System.out.println(ctx.getText());
           String[] aux = ctx.getText().split("");
           if (aux.length > 1 ) {
               if (!table.containsKey(aux[1])) {
@@ -55,8 +56,9 @@ public class metricador extends Java8BaseListener{
             }
           }
        }
-
+       /*Expresiones A -> a * b, A -> a / b, A-> a % b  */
        @Override public void enterMultiplicativeExpression(Java8Parser.MultiplicativeExpressionContext ctx) {
+            System.out.println(ctx.getText());
             String[] aux = ctx.getText().split("");
             if (aux.length > 1) {
                 if (!table.containsKey(aux[1])) {
@@ -71,12 +73,77 @@ public class metricador extends Java8BaseListener{
             }
         }
 
-        /*Expresiones de tipo A -> + a, A -> b */
+        /*Expresiones de tipo A -> + a, A -> - b */
         @Override public void enterUnaryExpression(Java8Parser.UnaryExpressionContext ctx) {
+            System.out.println(ctx.getText());
             String[] aux = ctx.getText().split("");
             if (aux.length > 1) {
                 if (!table.containsKey(aux[0])) {
                     table.put(aux[0], 1);
+                    N1++;
+                    n1++;
+                  } else {
+                    int num_ocurrences = table.get(aux[0]);
+                    table.put(aux[0], ++num_ocurrences);
+                    N1++;
+                }
+            }
+        }
+        /*Expresiones de tipo A -> ++ a */
+        @Override public void enterPreIncrementExpression(Java8Parser.PreIncrementExpressionContext ctx) {
+            System.out.println(ctx.getText());
+            String[] aux = ctx.getText().split("");
+            if (aux.length > 1) {
+                if (!table.containsKey(aux[0])) {
+                    table.put(aux[0], 1);
+                    N1++;
+                    n1++;
+                  } else {
+                    int num_ocurrences = table.get(aux[0]);
+                    table.put(aux[0], ++num_ocurrences);
+                    N1++;
+                }
+            }
+        }
+        /*Expresiones de tipo A -> -- a */
+        @Override public void enterPreDecrementExpression(Java8Parser.PreDecrementExpressionContext ctx) {
+            System.out.println(ctx.getText());
+            String[] aux = ctx.getText().split("");
+            if (aux.length > 1) {
+                if (!table.containsKey(aux[0])) {
+                    table.put(aux[0], 1);
+                    N1++;
+                    n1++;
+                  } else {
+                    int num_ocurrences = table.get(aux[0]);
+                    table.put(aux[0], ++num_ocurrences);
+                    N1++;
+                }
+            }
+        }
+        /*Expresiones de tipo A -> !a, A -> ~a */
+        @Override public void enterUnaryExpressionNotPlusMinus(Java8Parser.UnaryExpressionNotPlusMinusContext ctx) {
+            System.out.println(ctx.getText());
+            String[] aux = ctx.getText().split("");
+            if (aux.length > 1) {
+                if (!table.containsKey(aux[0])) {
+                    table.put(aux[0], 1);
+                    N1++;
+                    n1++;
+                  } else {
+                    int num_ocurrences = table.get(aux[0]);
+                    table.put(aux[0], ++num_ocurrences);
+                    N1++;
+                }
+            }
+        }
+
+        @Override public void enterPostIncrementExpression(Java8Parser.PostIncrementExpressionContext ctx) {
+            System.out.println(ctx.getText());
+            String[] aux = ctx.getText().split("");
+            if (aux.length > 1) {
+                if (!table.containsKey(aux[1])) {
+                    table.put(aux[1], 1);
                     N1++;
                     n1++;
                   } else {
@@ -85,7 +152,6 @@ public class metricador extends Java8BaseListener{
                     N1++;
                 }
             }
-        }
-        
+         }
 
 }
